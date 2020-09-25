58afc12a0277c5c0add67ba43c65e403c997866c
Eric Wise <ewise82@gmail.com>
2020-09-25 15:08:00 -0700

    Work on GET request

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
+      imageData: [],
     };
+  }
+
+  // eslint-disable-next-line camelcase
+  UNSAFE_componentWillMount() {
+    this.getImages(1);
+  }
+
+  getImages(listingId) {
+    axios.get(`/api/images/${listingId}`).then((data) => {
+      this.setState({ imageData: data.data });
+    }).catch((err) => {
+      console.log('Error getting image data', err);
+    });
   }
 
   render() {
