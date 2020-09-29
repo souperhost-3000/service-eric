998419f91f55c385b72bba9a76987a4b17e28c28
Eric Wise <ewise82@gmail.com>
2020-09-29 10:28:55 -0700

    Destructure imageData from props

--- client/src/photos.jsx
+++ client/src/photos.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-function photos(props) {
-  console.log('props', props);
-  const { images } = props.imageData;
+function photos({ imageData }) {
   return (
     <div>
-      <img src={images[12]} alt="" />
+      <img src={imageData.images[12]} alt="" />
     </div>
   );
 }
