66db5e42e306aff87256dc4e0e267edd7b9d56ba
Eric Wise <ewise82@gmail.com>
2020-10-02 13:49:06 -0700

    update mongo connection string

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb://localhost/image';
+const mongoUri = 'mongodb://localhost:27017/image';
 
 const db = mongoose.connect(mongoUri);
 
